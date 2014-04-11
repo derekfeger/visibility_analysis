e051397384a1985caf07baf331b3b37e2a998e54
derekfeger <derek.feger@gmail.com>
2014-04-11 15:19:29 -0400

    Once again fixed drive letter error

--- visibility_analysis.py
+++ visibility_analysis.py
@@ -38,25 +38,25 @@ for recordnumber in range(0,1):
 	# Select a record and create a new feature for just that billboard
 	arcpy.MakeFeatureLayer_management(input_points, "bb_r%r" % recordnumber) 
 	arcpy.SelectLayerByAttribute_management("bb_r%d" % recordnumber, "NEW_SELECTION", ' "FID" = %d ' % recordnumber)
-	arcpy.CopyFeatures_management("bb_r%r" % recordnumber, "E:\\independent_study\\visibility_analysis\\bboard_points\\bb_r%r" % recordnumber)
+	arcpy.CopyFeatures_management("bb_r%r" % recordnumber, "F:\\independent_study\\visibility_analysis\\bboard_points\\bb_r%r" % recordnumber)
 
 	# Create a 1000ft buffer around current billboard
-	arcpy.Buffer_analysis("E:\\independent_study\\visibility_analysis\\bboard_points\\bb_r%r.shp" % recordnumber, "E:\\independent_study\\visibility_analysis\\bboard_buffer\\bb_r%rbuf" % recordnumber, buffersize)
+	arcpy.Buffer_analysis("F:\\independent_study\\visibility_analysis\\bboard_points\\bb_r%r.shp" % recordnumber, "F:\\independent_study\\visibility_analysis\\bboard_buffer\\bb_r%rbuf" % recordnumber, buffersize)
 
 	# Clip the subset DEM to the individual billboard buffer
-	arcpy.Clip_management(subset_dem, "#", "E:\\independent_study\\visibility_analysis\\bboard_clip\\bb_r%rclip" % recordnumber, "E:\\independent_study\\visibility_analysis\\bboard_buffer\\bb_r%rbuf.shp" % recordnumber, "#", "ClippingGeometry")
+	arcpy.Clip_management(subset_dem, "#", "F:\\independent_study\\visibility_analysis\\bboard_clip\\bb_r%rclip" % recordnumber, "F:\\independent_study\\visibility_analysis\\bboard_buffer\\bb_r%rbuf.shp" % recordnumber, "#", "ClippingGeometry")
 
 	# Retrieves 3D analyst license, then creates a viewshed within individual DEM, then returns license to license manager
 	arcpy.CheckOutExtension("3D")
-	arcpy.Viewshed_3d("E:\\independent_study\\visibility_analysis\\bboard_clip\\bb_r%rclip" % recordnumber, "E:\\independent_study\\visibility_analysis\\bboard_points\\bb_r%r.shp" % recordnumber, "E:\\independent_study\\visibility_analysis\\bboard_viewshed\\bb_r%rvshd" % recordnumber)
+	arcpy.Viewshed_3d("F:\\independent_study\\visibility_analysis\\bboard_clip\\bb_r%rclip" % recordnumber, "F:\\independent_study\\visibility_analysis\\bboard_points\\bb_r%r.shp" % recordnumber, "F:\\independent_study\\visibility_analysis\\bboard_viewshed\\bb_r%rvshd" % recordnumber)
 	arcpy.CheckInExtension("3D")
 
 	# Pulls number of visible and not-visible pixels out of the table. Stores in variables vispix and nvispix, respectively.
-	with arcpy.da.SearchCursor("E:\\independent_study\\visibility_analysis\\bboard_viewshed\\bb_r%rvshd" % recordnumber, ("COUNT"), ' "Rowid" = 0') as cursor:
+	with arcpy.da.SearchCursor("F:\\independent_study\\visibility_analysis\\bboard_viewshed\\bb_r%rvshd" % recordnumber, ("COUNT"), ' "Rowid" = 0') as cursor:
 		for row in cursor:
 			nvispix = row[0]
 		
-	with arcpy.da.SearchCursor("E:\\independent_study\\visibility_analysis\\bboard_viewshed\\bb_r%rvshd" % recordnumber, ("COUNT"), ' "Rowid" = 1') as cursor:
+	with arcpy.da.SearchCursor("F:\\independent_study\\visibility_analysis\\bboard_viewshed\\bb_r%rvshd" % recordnumber, ("COUNT"), ' "Rowid" = 1') as cursor:
 		for row in cursor:
 			vispix = row[0]
 
