c3af5464d59cc46292b0382953f212f9ea7a21e6
derekfeger <derek.feger@gmail.com>
2014-04-11 13:53:52 -0400

    Changed drive letter for new computing environment

--- component_features/fulldem_clip.py
+++ component_features/fulldem_clip.py
@@ -2,16 +2,16 @@ import arcpy
 import os.path
 
 # Set the workspace
-arcpy.env.workspace = "E:\\independent_study\\visibility_analysis"
+arcpy.env.workspace = "F:\\independent_study\\visibility_analysis"
 
 # User Inputs (Ideally would be definable parameters instead of having to code in themselves)
 	# Should include input point features, full_dem, buffer size
-input_points = os.path.join('E:\\', 'independent_study', 'Billboardsdata_pghcityplanning', 'LamarSigns.shp')
-full_dem = "E:\\independent_study\\visibility_analysis\\fullcity_outputmosaic.tif"
+input_points = os.path.join('F:\\', 'independent_study', 'Billboardsdata_pghcityplanning', 'LamarSigns.shp')
+full_dem = "F:\\independent_study\\visibility_analysis\\fullcity_outputmosaic.tif"
 buffersize = "1000 Feet"
 
 # Buffers input point features to specified buffer size
-arcpy.Buffer_analysis(input_points, "E:\\independent_study\\visibility_analysis\\output_files\\va_rALL_buf.shp", buffersize)
+arcpy.Buffer_analysis(input_points, "F:\\independent_study\\visibility_analysis\\output_files\\va_rALL_buf.shp", buffersize)
 
 # Clips full DEM to extent of buffer output_file
-arcpy.Clip_management(full_dem, "#", "E:\\independent_study\\visibility_analysis\\dem_files\\clipped_dem.tif", "E:\\independent_study\\visibility_analysis\\output_files\\va_rALL_buf.shp", "#", "ClippingGeometry")
+arcpy.Clip_management(full_dem, "#", "F:\\independent_study\\visibility_analysis\\dem_files\\clipped_dem.tif", "F:\\independent_study\\visibility_analysis\\output_files\\va_rALL_buf.shp", "#", "ClippingGeometry")
--- visibility_analysis.py
+++ visibility_analysis.py
@@ -2,21 +2,21 @@ import arcpy
 import os.path
 
 # Set the workspace
-arcpy.env.workspace = "E:\\independent_study\\topo_efficiency"
+arcpy.env.workspace = "F:\\independent_study\\topo_efficiency"
 
 # User Inputs (Ideally would be definable parameters instead of having to code in themselves)
 	# Should include prepare_table
-input_points = "E:\\independent_study\\Billboardsdata_pghcityplanning\\LamarSigns.shp"
-full_dem = "E:\\independent_study\\visibility_analysis\\fullcity_outputmosaic.tif"
+input_points = "F:\\independent_study\\Billboardsdata_pghcityplanning\\LamarSigns.shp"
+full_dem = "F:\\independent_study\\visibility_analysis\\fullcity_outputmosaic.tif"
 buffersize = "1000 Feet"
-csv_output_file = "E:\\independent_study\\topo_efficiency\\topo_efficiency.csv"
+csv_output_file = "F:\\independent_study\\topo_efficiency\\topo_efficiency.csv"
 
 # Set global variables, variables for produced files
 recordnumber = 0
 prepare_table = True
 vispix = 0
 nvispix = 0
-subset_dem = "E:\\independent_study\\visibility_analysis\\dem_files\\clipped_dem.tif"
+subset_dem = "F:\\independent_study\\visibility_analysis\\dem_files\\clipped_dem.tif"
 
 # Set local functions
 def table_prep(tablefile):
@@ -37,25 +37,25 @@ for recordnumber in range(0,1):
 	# Select a record and create a new feature for just that billboard
 	arcpy.MakeFeatureLayer_management(input_points, "bb_r%r" % recordnumber) 
 	arcpy.SelectLayerByAttribute_management("bb_r%d" % recordnumber, "NEW_SELECTION", ' "FID" = %d ' % recordnumber)
-	arcpy.CopyFeatures_management("bb_r%r" % recordnumber, "E:\\independent_study\\topo_efficiency\\bboard_points\\bb_r%r" % recordnumber)
+	arcpy.CopyFeatures_management("bb_r%r" % recordnumber, "F:\\independent_study\\topo_efficiency\\bboard_points\\bb_r%r" % recordnumber)
 
 	# Create a 1000ft buffer around current billboard
-	arcpy.Buffer_analysis("E:\\independent_study\\topo_efficiency\\bboard_points\\bb_r%r.shp" % recordnumber, "E:\\independent_study\\topo_efficiency\\bboard_buffer\\bb_r%rbuf" % recordnumber, buffersize)
+	arcpy.Buffer_analysis("F:\\independent_study\\topo_efficiency\\bboard_points\\bb_r%r.shp" % recordnumber, "F:\\independent_study\\topo_efficiency\\bboard_buffer\\bb_r%rbuf" % recordnumber, buffersize)
 
 	# Clip the subset DEM to the individual billboard buffer
-	arcpy.Clip_management("E:\\independent_study\\topo_efficiency\\fullclip_test", "#", "E:\\independent_study\\topo_efficiency\\bboard_clip\\bb_r%rclip" % recordnumber, "E:\\independent_study\\topo_efficiency\\bboard_buffer\\bb_r%rbuf.shp" % recordnumber, "#", "ClippingGeometry")
+	arcpy.Clip_management("F:\\independent_study\\topo_efficiency\\fullclip_test", "#", "F:\\independent_study\\topo_efficiency\\bboard_clip\\bb_r%rclip" % recordnumber, "F:\\independent_study\\topo_efficiency\\bboard_buffer\\bb_r%rbuf.shp" % recordnumber, "#", "ClippingGeometry")
 
 	# Retrieves 3D analyst license, then creates a viewshed within individual DEM, then returns license to license manager
 	arcpy.CheckOutExtension("3D")
-	arcpy.Viewshed_3d("E:\\independent_study\\topo_efficiency\\bboard_clip\\bb_r%rclip" % recordnumber, "E:\\independent_study\\topo_efficiency\\bboard_points\\bb_r%r.shp" % recordnumber, "E:\\independent_study\\topo_efficiency\\bboard_viewshed\\bb_r%rvshd" % recordnumber)
+	arcpy.Viewshed_3d("F:\\independent_study\\topo_efficiency\\bboard_clip\\bb_r%rclip" % recordnumber, "F:\\independent_study\\topo_efficiency\\bboard_points\\bb_r%r.shp" % recordnumber, "F:\\independent_study\\topo_efficiency\\bboard_viewshed\\bb_r%rvshd" % recordnumber)
 	arcpy.CheckInExtension("3D")
 
 	# Pulls number of visible and not-visible pixels out of the table. Stores in variables vispix and nvispix, respectively.
-	with arcpy.da.SearchCursor("E:\\independent_study\\topo_efficiency\\bboard_viewshed\\bb_r%rvshd" % recordnumber, ("COUNT"), ' "Rowid" = 0') as cursor:
+	with arcpy.da.SearchCursor("F:\\independent_study\\topo_efficiency\\bboard_viewshed\\bb_r%rvshd" % recordnumber, ("COUNT"), ' "Rowid" = 0') as cursor:
 		for row in cursor:
 			nvispix = row[0]
 		
-	with arcpy.da.SearchCursor("E:\\independent_study\\topo_efficiency\\bboard_viewshed\\bb_r%rvshd" % recordnumber, ("COUNT"), ' "Rowid" = 1') as cursor:
+	with arcpy.da.SearchCursor("F:\\independent_study\\topo_efficiency\\bboard_viewshed\\bb_r%rvshd" % recordnumber, ("COUNT"), ' "Rowid" = 1') as cursor:
 		for row in cursor:
 			vispix = row[0]
 
